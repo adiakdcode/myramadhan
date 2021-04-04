f8e8975574d3fc0b78776b87cee8366ef0d7969d
Adia Wahyu Dwi Prasetya <54004272+adiakdcode@users.noreply.github.com>
2021-04-04 20:09:07 +0700

    WWW

---
+++ assets/js/mysrcipt.js
@@ -0,0 +1,25 @@
+const blink = 270; 
+var www = setInterval(function () {
+    var ele = document.getElementById('countdown');
+    ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
+}, blink);
+
+const blink2 = 198; 
+var www2 = setInterval(function () {
+    var ele2 = document.getElementById('countdown2');
+    ele2.style.visibility = (ele2.style.visibility == 'hidden' ? '' : 'hidden');
+}, blink2);
+
+
+/*new Typed('#typed',{
+    strings : ['Selamat menjalani ibadah Puasa, Jaga Mata, Jaga Mulut, Jaga Telinga, Isi Galeri jangan lupa awkwk'],
+    typeSpeed : 40,
+    delaySpeed : 70,
+    loop : true
+});*/
+new Typed('#typedq',{
+    strings : ['Sekedar memberitahukan, Bulan Ramadhan Kurang : '],
+    typeSpeed : 70,
+    delaySpeed : 70,
+    loop : true
+});
